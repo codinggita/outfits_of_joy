081dff0c3c9ad7085dc6c946c06c4685ef63b37f
PARTH-JADAV20 <parthjadav.cg@gmail.com>
2025-02-02 17:10:09 +0530

    designed womens outfits categories to go to that outfit page

--- Client/src/component/Homepage.jsx
+++ Client/src/component/Homepage.jsx
@@ -4,6 +4,9 @@ import img1 from '../assets/Group 23.png'
 import img2 from '../assets/s1.png'
 import img3 from '../assets/i1.png'
 import img4 from '../assets/t1.png'
+import img5 from '../assets/l1.png'
+import img6 from '../assets/a1.png'
+import img7 from '../assets/g1.png'
 
 export default function Homepage() {
   return (
@@ -26,6 +29,23 @@ export default function Homepage() {
                     <img src={img4} alt="" />
                     <p>Tuxedo</p>
                 </div>
+            </div>
+            <div id='hpwomensection'>
+                <div>
+                    <img src={img5} alt="" />
+                    <p>Lehenga</p>
+                </div>
+                <div>
+                    <img src={img6} alt="" />
+                    <p>Anarkali</p>
+                </div>
+                <div>
+                    <img src={img7} alt="" />
+                    <p>Gown</p>
+                </div>
+            </div>
+            <div id='hpinstruction'>
+                <img src={img1} alt="" />
             </div>
         </section>
     </main>
